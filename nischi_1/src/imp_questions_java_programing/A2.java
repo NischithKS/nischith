8b4c9bc75e6c3a7fb776734dfc955bd66092344f
techi <techi@Nischith.lan>
2026-01-18 09:34:08 +0530

    ok to code

---
+++ nischi_1/src/imp_questions_java_programing/A2.java
@@ -0,0 +1,33 @@
+package imp_questions_java_programing;
+
+public class A2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //Comparing 2 numbers using IF CONDITION
+		
+	/*	
+	 int a=1, b=3;
+	 if (a==b) {
+		 System.out.println("a is equal to b");
+	 }
+		 else {
+			 System.out.println("a not equal to b");
+		 
+	 }
+		
+	}
+
+}
+*/
+		
+        int a = 15, b = 20;
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } else if (a < b) {
+            System.out.println(b + " is greater than " + a);
+        } else {
+            System.out.println(a + " is equal to " + b);
+        }
+    }
+}
---
+++ nischi_1/src/nischi_java_programe_2/Class_2.java
@@ -0,0 +1,17 @@
+package nischi_java_programe_2;
+
+public class Class_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char D='1' , N='2';
+		int n1=6;
+		
+		System.out.println(D);
+		System.out.println(N);
+		System.out.println(n1);
+
+
+	}
+
+}
