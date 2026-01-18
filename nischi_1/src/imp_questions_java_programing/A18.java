893d2c3b82a43866fda50fbd60eb86f9c3ec0aba
techi <techi@Nischith.lan>
2026-01-18 09:07:20 +0530

    my few java codes

---
+++ nischi_1/src/imp_questions_java_programing/A18.java
@@ -0,0 +1,14 @@
+package imp_questions_java_programing;
+
+
+
+	// Multiplication Program (print multiplication table for a number)
+
+public class A18 {
+	    public static void main(String[] args) {
+	        int number = 5;  // hardcoded number
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(number + " x " + i + " = " + (number * i));
+	        }
+	    }
+	}
